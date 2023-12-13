4a2ccbe08a07a5f580077c8144c5b0e4dd5bdc4c
gaurav3278 <gauravrai54152@gmail.com>
2023-12-13 15:59:34 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+},{timestamps: true})
+
+export default mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import session from 'express-session'
+import passport from 'passport'
+import { connectDB } from './config/database.js'
+import MongoStore from 'connect-mongo'
+
+const app = express()
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: 'none',
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24
+    },
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        ttl: 24 * 60 * 60 * 1000 
+    })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+const startServer = async()=>{
+    try{
+        await connectDB()
+        app.listen(5000, ()=>{
+            console.log(`Server running on port 5000`)
+        })
+    }catch(err){
+        console.error(err)
+    }
+}
+
+startServer()
+
+mongoose.connection.on('connected', ()=>{
+    console.log('MongoDB connected')
+})
+
+mongoose.connection.on('disconnected', (err)=>{
+    console.error('MongoDB disconnected')
+})
