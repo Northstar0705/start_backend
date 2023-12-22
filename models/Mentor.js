e89e810c8622d9a7f45aa78356a55ce7baeaeb08
gaurav3278 <gauravrai54152@gmail.com>
2023-12-22 12:50:17 +0530

    changes by gaurav

---
+++ models/Mentor.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+
+const mentorSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    jobTitle:{
+        type:String,
+        required: true
+    },
+    company:{
+        type:String,
+        required: true
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    skills:[{
+        type:String,
+        required: true
+    }],
+    bio:{
+        type:String,
+        required: true
+    },
+    profilePicture:{
+        type:String,
+        required: true
+    },
+    linkedin:{
+        type:String,
+        required: true
+    },
+    twitter:{
+        type : String
+    },
+    website:{
+        type : String
+    },
+    introVideo:{
+        type : String
+    },
+    articles:{
+        type : String
+    },
+    whyMentor:{
+        type : String
+    },
+    greatestAcheivement:{
+        type : String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+},{timestamps:true});
+
+export default mongoose.model("Mentor", mentorSchema);
